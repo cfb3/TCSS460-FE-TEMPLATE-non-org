8199ae28a0919367d9ab13487a23e9567a52adc0
cfb3 <cfb3@uw.edu>
2025-05-12 09:09:13 -0700

    Linted

--- src/components/logo/LogoIcon.tsx
+++ src/components/logo/LogoIcon.tsx
@@ -1,4 +1,3 @@
-
 // next
 import Image from 'next/image';
 
@@ -7,9 +6,7 @@ import Image from 'next/image';
 const logoIcon = 'assets/images/logo-icon.svg';
 
 export default function LogoIcon() {
-
   return (
     <Image src={logoIcon} alt="UW" width={55} height={55} />
-
   );
 }
