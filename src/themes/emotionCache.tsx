251e99567665fbccc1b6d13d150c147a06cd4008
cfb3 <cfb3@uw.edu>
2025-05-29 14:10:53 -0700

    revert

--- src/themes/emotionCache.tsx
+++ src/themes/emotionCache.tsx
@@ -14,7 +14,7 @@ export type NextAppDirEmotionCacheProviderProps = {
   /** This is the options passed to createCache() from 'import createCache from "@emotion/cache"' */
   options: Omit<OptionsOfCreateCache, 'insertionPoint'>;
   /** By default <CacheProvider /> from 'import { CacheProvider } from "@emotion/react"' */
-  CacheProvider?: Provider<EmotionCache | null>;
+  CacheProvider?: Provider<EmotionCache>;
   children: ReactNode;
 };
 
